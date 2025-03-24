0971071262a360db1a5645c0aadfb625094670f1
Harry <harry_woodgate11@hotmail.co.uk>
2025-03-24 10:57:01 +0000

    Add a set method to HashMap

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class HashMap {
   capacity = 16;
-  load_factor = (this.capacity / 0.75);
-  array = [];
+  loadFactor = (this.capacity / 0.75);
+  hashMap = [];
 
   hash(key) {
     let hashCode = 0;
@@ -13,8 +13,43 @@ class HashMap {
     hashCode = hashCode % this.capacity;
     return hashCode;
   }
+
+  set(key, value) {
+
+    if (this.hashMap.length >= this.loadFactor) {
+      this.capacity = this.capacity * 2;
+    }
+
+    const hashCode = this.hash(key);
+
+    for (let bucket of this.hashMap) {
+      if (bucket[0] === hashCode) {
+        bucket[1] = value;
+        // console.log(this.hashMap);
+        return
+      }
+    }
+
+    const array = [hashCode, value];
+    this.hashMap.splice(hashCode, 1, array);
+
+    // console.log(this.hashMap)
+  }
 }
 
-const hash_map = new HashMap();
+const hashMap = new HashMap();
 
-console.log(hash_map.hash("penis"));
+hashMap.set('apple', 'red')
+hashMap.set('banana', 'yellow')
+hashMap.set('carrot', 'orange')
+hashMap.set('dog', 'brown')
+hashMap.set('elephant', 'gray')
+hashMap.set('frog', 'green')
+hashMap.set('grape', 'purple')
+hashMap.set('hat', 'black')
+hashMap.set('ice cream', 'white')
+hashMap.set('jacket', 'blue')
+hashMap.set('kite', 'pink')
+hashMap.set('lion', 'golden')
+
+console.log(hashMap.hashMap)
