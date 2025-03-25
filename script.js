c99578fe13d28322adcf2dd5d7952850fdc749f9
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-03-25 10:38:06 +0000

    Add a working get method.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class HashMap {
   capacity = 16;
-  loadFactor = (this.capacity / 0.75);
+  loadFactor = this.capacity / 0.75;
   hashMap = [];
 
   hash(key) {
@@ -14,42 +14,65 @@ class HashMap {
   }
 
   set(key, value) {
-
     if (this.hashMap.length >= this.loadFactor) {
       this.capacity = this.capacity * 2;
     }
 
     const hashCode = this.hash(key);
     const bucket = hashCode % this.capacity;
-    const keyValuepair = {key: hashCode, value: value};
+    const keyValuepair = { key: hashCode, value: value };
 
-    if (this.hashMap.length === 0 || this.hashMap[hashCode % this.capacity] == undefined) {
+    if (
+      this.hashMap.length === 0 ||
+      this.hashMap[hashCode % this.capacity] == undefined
+    ) {
       this.hashMap[bucket] = [keyValuepair];
-      return
-    }
-    else if (this.hashMap[bucket] != 0) {
+      return;
+    } else if (this.hashMap[bucket] != 0) {
       keyValuepair.nextNode = null;
-      this.hashMap[bucket][this.hashMap[bucket].length - 1].nextNode = keyValuepair;
+      this.hashMap[bucket][this.hashMap[bucket].length - 1].nextNode =
+        keyValuepair;
       this.hashMap[bucket].push(keyValuepair);
-      return
+      return;
     }
+  }
+
+  get(key) {
+    key = this.hash(key);
+
+    for (const bucket of this.hashMap) {
+      if (bucket == undefined) {
+        continue;
+      } else if (bucket.length > 1) {
+        let head = bucket[0];
+        while (head != null) {
+          if (head.key === key) {
+            return head.value;
+          }
+          head = head.nextNode;
+        }
+      } else if (bucket[0].key === key) {
+        return bucket[0].value;
+      }
+    }
+
+    return null;
   }
 }
 
-const hashMap = new HashMap();
+// const hashMap = new HashMap();
 
-hashMap.set('apple', 'red')
-hashMap.set('banana', 'yellow')
-hashMap.set('carrot', 'orange')
-hashMap.set('dog', 'brown')
-hashMap.set('elephant', 'gray')
-hashMap.set('frog', 'green')
-hashMap.set('grape', 'purple')
-hashMap.set('hat', 'black')
-hashMap.set('ice cream', 'white')
-hashMap.set('jacket', 'blue')
-hashMap.set('kite', 'pink')
-hashMap.set('lion', 'golden')
+// hashMap.set("apple", "red");
+// hashMap.set("banana", "yellow");
+// hashMap.set("carrot", "orange");
+// hashMap.set("dog", "brown");
+// hashMap.set("elephant", "gray");
+// hashMap.set("frog", "green");
+// hashMap.set("grape", "purple");
+// hashMap.set("hat", "black");
+// hashMap.set("ice cream", "white");
+// hashMap.set("jacket", "blue");
+// hashMap.set("kite", "pink");
+// hashMap.set("lion", "golden");
 
-console.log(hashMap.hashMap);
-
+// console.log(hashMap.get("apple"));
