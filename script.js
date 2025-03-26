3cec70695da9c89ed8d8da7196bec4b99e6deb24
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-03-26 11:46:13 +0000

    Tidy up the document.

--- script.js
+++ script.js
@@ -217,21 +217,4 @@ class HashMap {
   }
 }
 
-const hashMap = new HashMap();
 
-hashMap.set('apple', 'red')
-hashMap.set('banana', 'yellow')
-hashMap.set('carrot', 'orange')
-hashMap.set('dog', 'brown')
-hashMap.set('elephant', 'gray')
-hashMap.set('frog', 'green')
-hashMap.set('grape', 'purple')
-hashMap.set('hat', 'black')
-hashMap.set('ice cream', 'white')
-hashMap.set('jacket', 'blue')
-hashMap.set('kite', 'pink')
-hashMap.set('lion', 'golden')
-
-
-
-console.log(hashMap.hashMap);
