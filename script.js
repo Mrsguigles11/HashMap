090583185f1a7d0dded9aaf6de2683817fbddbca
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-03-26 12:06:59 +0000

    Remove testing code at bottom of document.

--- script.js
+++ script.js
@@ -217,19 +217,3 @@ class HashMap {
   }
 }
 
-const hashMap = new HashMap();
-
-hashMap.set('apple', 'red')
-hashMap.set('banana', 'yellow')
-hashMap.set('carrot', 'orange')
-hashMap.set('dog', 'brown')
-hashMap.set('elephant', 'gray')
-hashMap.set('frog', 'green')
-hashMap.set('grape', 'purple')
-hashMap.set('hat', 'black')
-hashMap.set('ice cream', 'white')
-hashMap.set('jacket', 'blue')
-hashMap.set('kite', 'pink')
-hashMap.set('lion', 'golden')
-
-console.log(hashMap.hashMap)
